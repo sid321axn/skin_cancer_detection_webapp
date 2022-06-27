29c04d281d31ac21d663c89e6df84d6a861ebb40
sid321axn <sid321axn@gmail.com>
2022-06-27 09:09:18 +0530

    structure change

--- app.py
+++ app.py
@@ -1,25 +1,24 @@
-﻿from __future__ import division, print_function
-# coding=utf-8
-import sys
-import os, shutil
-import glob
-import re
+﻿"""
+Skin cancer detection web app
+
+designed and developed by Wisdom ML
+"""
+# Importing essential python libraries
+
+from __future__ import division, print_function
+import os
 import numpy as np
-from IPython.display import Image as im1, display
-from tensorflow.keras.preprocessing import image as im
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
-import tensorflow as tf
 # Keras
-from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 from PIL import Image
 # Flask utils
-from flask import Flask, redirect, url_for, request, render_template,send_from_directory
+from flask import Flask,  url_for, request, render_template,send_from_directory
 from werkzeug.utils import secure_filename
-from gevent.pywsgi import WSGIServer
-from werkzeug.security import safe_join
+from heatmap import save_and_display_gradcam,make_gradcam_heatmap
+
+
+
 os.environ["CUDA_VISIBLE_DEVICES"]="-1"
 # Define a flask app
 app = Flask(__name__, static_url_path='')
@@ -37,15 +36,24 @@ model = load_model(MODEL_PATH)
 print('Model loaded. Start serving...')
 
 
+class_dict = {0:"Basal_Cell_Carcinoma (Cancer)",
+             1:"Melanoma (Cancer)",
+             2:"Nevus (Non-Cancerous)"}
+
 @app.route('/uploads/<filename>')
 def upload_img(filename):
     
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
-
+        
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
 
 
 
 def model_predict(img_path, model):
+    
     img = Image.open(img_path).resize((224,224)) #target_size must agree with what the trained model expects!!
 
     # Preprocessing the image
@@ -53,16 +61,13 @@ def model_predict(img_path, model):
     img = np.expand_dims(img, axis=0)
     img = img.astype('float32')/255
    
-    preds = model.predict(img)
-    pred = np.argmax(preds,axis = 1)
-    print(preds)
-    return pred,img
-
-
-@app.route('/', methods=['GET'])
-def index():
-    # Main page
-    return render_template('index.html')
+    preds = model.predict(img)[0]
+    prediction = sorted(
+      [(class_dict[i], round(j*100, 2)) for i, j in enumerate(preds)],
+      reverse=True,
+      key=lambda x: x[1]
+  )
+    return prediction,img
 
 
 @app.route('/predict', methods=['GET', 'POST'])
@@ -81,120 +86,19 @@ def predict():
         file_name=os.path.basename(file_path)
         # Make prediction
         pred,img = model_predict(file_path, model)
-        #os.remove(file_path)#removes file from the server after prediction has been returned
-
-        # Arrange the correct return according to the model. 
-		# In this model 1 is Pneumonia and 0 is Normal.
-        str1 = 'Basal Cel Carcinoma'
-        str2 = 'Melanoma'
-        str3 = 'Nevus'
         
         last_conv_layer_name = "block_16_depthwise"
         heatmap = make_gradcam_heatmap(img, model, last_conv_layer_name)
         fname=save_and_display_gradcam(file_path, heatmap)
-        if pred[0] == 0:
-            res=str1
-            
-        elif pred[0] == 1:
-            res= str2
-        else:
-            res= str3
         
         
-    return render_template('predict.html',file_name=file_name, heatmap_file=fname,result=res)
+        
+    return render_template('predict.html',file_name=file_name, heatmap_file=fname,result=pred)
 
 
-def save_and_display_gradcam(img_path, heatmap, cam_path="cam.jpg", alpha=0.4):
-    # Load the original image
-    # fname=img_path.split('.')[-1]
-    file_name=os.path.basename(img_path)
-    img = tf.keras.preprocessing.image.load_img(img_path)
-    img = tf.keras.preprocessing.image.img_to_array(img)
-    # img = im.open(img_path).resize((224,224)) #target_size must agree with what the trained model expects!!
-    
-    # # Preprocessing the image
-    # img = im.img_to_array(img)
-    # img = np.expand_dims(img, axis=0)
-    # img = img.astype('float32')/255
-
-    # Rescale heatmap to a range 0-255
-    heatmap = np.uint8(255 * heatmap)
-
-    # Use jet colormap to colorize heatmap
-    jet = cm.get_cmap("jet")
-
-    # Use RGB values of the colormap
-    jet_colors = jet(np.arange(256))[:, :3]
-    jet_heatmap = jet_colors[heatmap]
-
-    # Create an image with RGB colorized heatmap
-    jet_heatmap = tf.keras.preprocessing.image.array_to_img(jet_heatmap)
-    jet_heatmap = jet_heatmap.resize((img.shape[1], img.shape[0]))
-    jet_heatmap = tf.keras.preprocessing.image.img_to_array(jet_heatmap)
-
-    # Superimpose the heatmap on original image
-    superimposed_img = jet_heatmap * alpha + img
-    superimposed_img = tf.keras.preprocessing.image.array_to_img(superimposed_img)
-    file_name=file_name+"_"+cam_path
-    basepath = os.path.dirname(__file__)
-    cam_path = os.path.join(
-            basepath, 'uploads', secure_filename(file_name))
-    # Save the superimposed image
-    
-    
-    superimposed_img.save(cam_path)
-    
-    return file_name
-
-    # Display Grad CAM
-   # display(Image(cam_path))
-
-def get_img_array(img_path, size):
-    # `img` is a PIL image of size 299x299
-    img = tf.keras.preprocessing.image.load_img(img_path, target_size=size)
-    # `array` is a float32 Numpy array of shape (299, 299, 3)
-    array = tf.keras.preprocessing.image.img_to_array(img)
-    # We add a dimension to transform our array into a "batch"
-    # of size (1, 299, 299, 3)
-    array = np.expand_dims(array, axis=0)
-    return array
 
 
-def make_gradcam_heatmap(img_array, model, last_conv_layer_name, pred_index=None):
-    # First, we create a model that maps the input image to the activations
-    # of the last conv layer as well as the output predictions
-    grad_model = tf.keras.models.Model(
-        [model.inputs], [model.get_layer(last_conv_layer_name).output, model.output]
-    )
-
-    # Then, we compute the gradient of the top predicted class for our input image
-    # with respect to the activations of the last conv layer
-    with tf.GradientTape() as tape:
-        last_conv_layer_output, preds = grad_model(img_array)
-        if pred_index is None:
-            pred_index = tf.argmax(preds[0])
-        class_channel = preds[:, pred_index]
-
-    # This is the gradient of the output neuron (top predicted or chosen)
-    # with regard to the output feature map of the last conv layer
-    grads = tape.gradient(class_channel, last_conv_layer_output)
-
-    # This is a vector where each entry is the mean intensity of the gradient
-    # over a specific feature map channel
-    pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
-
-    # We multiply each channel in the feature map array
-    # by "how important this channel is" with regard to the top predicted class
-    # then sum all the channels to obtain the heatmap class activation
-    last_conv_layer_output = last_conv_layer_output[0]
-    heatmap = last_conv_layer_output @ pooled_grads[..., tf.newaxis]
-    heatmap = tf.squeeze(heatmap)
-
-    # For visualization purpose, we will also normalize the heatmap between 0 & 1
-    heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
-    return heatmap.numpy()
-
-    #this section is used by gunicorn to serve the app on Heroku
+    #this section is used by gunicorn to serve the app on Azure and localhost
 if __name__ == '__main__':
         app.run(debug=True, host="localhost", port=8080)
     #uncomment this section to serve the app locally with gevent at:  http://localhost:5000
---
+++ heatmap.py
@@ -0,0 +1,95 @@
+import os
+import tensorflow as tf
+import numpy as np
+import matplotlib.cm as cm
+from werkzeug.utils import secure_filename
+
+def save_and_display_gradcam(img_path, heatmap, cam_path="cam.jpg", alpha=0.4):
+    # Load the original image
+    # fname=img_path.split('.')[-1]
+    file_name=os.path.basename(img_path)
+    img = tf.keras.preprocessing.image.load_img(img_path)
+    img = tf.keras.preprocessing.image.img_to_array(img)
+    # img = im.open(img_path).resize((224,224)) #target_size must agree with what the trained model expects!!
+    
+    # # Preprocessing the image
+    # img = im.img_to_array(img)
+    # img = np.expand_dims(img, axis=0)
+    # img = img.astype('float32')/255
+
+    # Rescale heatmap to a range 0-255
+    heatmap = np.uint8(255 * heatmap)
+
+    # Use jet colormap to colorize heatmap
+    jet = cm.get_cmap("jet")
+
+    # Use RGB values of the colormap
+    jet_colors = jet(np.arange(256))[:, :3]
+    jet_heatmap = jet_colors[heatmap]
+
+    # Create an image with RGB colorized heatmap
+    jet_heatmap = tf.keras.preprocessing.image.array_to_img(jet_heatmap)
+    jet_heatmap = jet_heatmap.resize((img.shape[1], img.shape[0]))
+    jet_heatmap = tf.keras.preprocessing.image.img_to_array(jet_heatmap)
+
+    # Superimpose the heatmap on original image
+    superimposed_img = jet_heatmap * alpha + img
+    superimposed_img = tf.keras.preprocessing.image.array_to_img(superimposed_img)
+    file_name=file_name+"_"+cam_path
+    basepath = os.path.dirname(__file__)
+    cam_path = os.path.join(
+            basepath, 'uploads', secure_filename(file_name))
+    # Save the superimposed image
+    
+    
+    superimposed_img.save(cam_path)
+    
+    return file_name
+
+    # Display Grad CAM
+   # display(Image(cam_path))
+
+def get_img_array(img_path, size):
+    # `img` is a PIL image of size 299x299
+    img = tf.keras.preprocessing.image.load_img(img_path, target_size=size)
+    # `array` is a float32 Numpy array of shape (299, 299, 3)
+    array = tf.keras.preprocessing.image.img_to_array(img)
+    # We add a dimension to transform our array into a "batch"
+    # of size (1, 299, 299, 3)
+    array = np.expand_dims(array, axis=0)
+    return array
+
+
+def make_gradcam_heatmap(img_array, model, last_conv_layer_name, pred_index=None):
+    # First, we create a model that maps the input image to the activations
+    # of the last conv layer as well as the output predictions
+    grad_model = tf.keras.models.Model(
+        [model.inputs], [model.get_layer(last_conv_layer_name).output, model.output]
+    )
+
+    # Then, we compute the gradient of the top predicted class for our input image
+    # with respect to the activations of the last conv layer
+    with tf.GradientTape() as tape:
+        last_conv_layer_output, preds = grad_model(img_array)
+        if pred_index is None:
+            pred_index = tf.argmax(preds[0])
+        class_channel = preds[:, pred_index]
+
+    # This is the gradient of the output neuron (top predicted or chosen)
+    # with regard to the output feature map of the last conv layer
+    grads = tape.gradient(class_channel, last_conv_layer_output)
+
+    # This is a vector where each entry is the mean intensity of the gradient
+    # over a specific feature map channel
+    pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
+
+    # We multiply each channel in the feature map array
+    # by "how important this channel is" with regard to the top predicted class
+    # then sum all the channels to obtain the heatmap class activation
+    last_conv_layer_output = last_conv_layer_output[0]
+    heatmap = last_conv_layer_output @ pooled_grads[..., tf.newaxis]
+    heatmap = tf.squeeze(heatmap)
+
+    # For visualization purpose, we will also normalize the heatmap between 0 & 1
+    heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
+    return heatmap.numpy()
